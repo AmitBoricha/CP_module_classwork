66bde0322a62838462d03ffbb247c53fe9d436ca
AmitBoricha <borichaamit1@gmail.com>
2024-10-29 12:23:08 +0100

    added list and dict excercises

--- lecture_4.py
+++ lecture_4.py
@@ -18,3 +18,47 @@ print(id(z))   #140733933877816
 #if we change the value of x, the id changes
 x = 10
 print(id(x))    #140734747900632
+
+
+
+#datastructure excercises
+
+def create_int_list(n):
+    li = []
+    for i in range(0,n):
+        li.append(i)
+    return li
+
+x = create_int_list(10)
+print("list x: ", x)
+
+print("maximum number: ",max(x))
+print("minimum number: ",min(x))
+
+#write a program that takes list of dict with keys name and age and returns a list of names sorted by age in ascending order
+li = [{"name": "a","age":10},{"name": "b","age":20},{"name": "c","age":15}]
+def sorted_dict(li):
+    ages = []
+    names = []
+    for i in li:
+        print(i)
+        ages.append(i['age'])
+    ages.sort()
+    for x in ages:
+        for j in li:
+            if x == j['age']:
+                names.append(j['name'])
+
+    return names
+
+
+ascending_names_by_age = sorted_dict(li)
+print(ascending_names_by_age)
+
+
+
+
+
+
+
+

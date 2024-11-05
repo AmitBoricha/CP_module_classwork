5d4b8d52bd3148c0dc801011228fbd9915a78e4d
AmitBoricha <borichaamit1@gmail.com>
2024-11-05 12:23:12 +0100

    raise Expection excercise

--- lecture_5.py
+++ lecture_5.py
@@ -19,3 +19,30 @@ for i in range(0,num_of_characters):
     print(random_string[i])
 
 
+#Excercise 3
+
+lst1 = [3,7,6,8,"a",9,11,15,25]
+lst2 = []
+lst3 = []
+for i in lst1:
+    lst2.append(i**2)
+
+print(lst2)
+
+
+#excercise 4
+
+lst1 = [3,7,6,8,"a",9,11,15,25]
+
+for i in lst1:
+    if type(i) == str:
+        raise Exception( i + " was a string, not an integer")
+    else:
+        lst3.append(i ** 2)
+
+print(lst3)
+
+
+
+
+

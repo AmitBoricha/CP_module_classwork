79a08c0880d99ac7301d229eb5b4f2c06b204ae1
AmitBoricha <borichaamit1@gmail.com>
2024-11-12 12:11:50 +0100

    request module excercise

--- lecture_5.py
+++ lecture_5.py
@@ -21,26 +21,75 @@ for i in range(0,num_of_characters):
 
 #Excercise 3
 
-lst1 = [3,7,6,8,"a",9,11,15,25]
-lst2 = []
-lst3 = []
-for i in lst1:
-    lst2.append(i**2)
-
-print(lst2)
+# lst1 = [3,7,6,8,"a",9,11,15,25]
+# lst2 = []
+# lst3 = []
+# for i in lst1:
+#     lst2.append(i**2)
+#
+# print(lst2)
 
 
 #excercise 4
 
-lst1 = [3,7,6,8,"a",9,11,15,25]
+# lst1 = [3,7,6,8,"a",9,11,15,25]
+#
+# for i in lst1:
+#     if type(i) == str:
+#         raise Exception( i + " was a string, not an integer")
+#     else:
+#         lst3.append(i ** 2)
+#
+# print(lst3)
 
-for i in lst1:
-    if type(i) == str:
-        raise Exception( i + " was a string, not an integer")
+#excercise 5
+
+def square(number):
+    return number**2
+
+def max_num(num1,num2):
+    if num1 > num2:
+        return num1
     else:
-        lst3.append(i ** 2)
+        return num2
 
-print(lst3)
+def calculate_tax(salary):
+    if salary <= 20000:
+        return 0
+    elif salary > 20000 & salary <= 50000:
+        return 0.10*salary
+    elif salary >50000 & salary <=10000:
+        return 0.20 * salary
+    else:
+        return 0.30 * salary
+
+
+def reverse_string(string):
+    reversed_string = ""
+    for i in range(len(string),0):
+        reversed_string = reversed_string + string[i]
+
+
+def find_average(list_of_numbers):
+    n = len(list_of_numbers)
+    sum = 0
+    for i in list_of_numbers:
+        sum = sum + i
+    avg = sum / n
+
+import requests
+
+def currency_exchange(date):
+    url = f"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/currencies/eur.json"
+
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+currency_exchange("latest")
+
+
+
 
 
 

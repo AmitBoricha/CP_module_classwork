c726014355ebff85ebec51f57e2b587707da7762
AmitBoricha <borichaamit1@gmail.com>
2024-11-12 12:30:51 +0100

    added excercise 1and 2

--- lecture_6.py
+++ lecture_6.py
@@ -1,16 +1,39 @@
-#excercise1
+#excercise
 
 class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
 
-    def myfunc(self):
-        print("Hello my name is ",self.name)
+    def greet(self):
+        print("Hello, my name is "+ self.name + " and I am " + str(self.age) + " years old.")
 
 p1 = Person("Amit",32)
 
 print(p1.name)
 print(p1.age)
 print(p1)
-p1.myfunc()
+p1.greet()
+
+
+#excercise2
+
+class Rectangle:
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+
+    def area_of_rectangle(self):
+        area = self.width * self.height
+        return area
+
+    def perimeter_of_rectangle(self):
+        perimeter = 2 * (self.width + self.height)
+        return perimeter
+
+r1 = Rectangle(10,20)
+
+print(r1.area_of_rectangle())
+print(r1.perimeter_of_rectangle())
+
+
